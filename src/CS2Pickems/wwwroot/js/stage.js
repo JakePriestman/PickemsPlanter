1f429c5eaaecbfebd20aaf15f495f8282a2b5d24
jake priestman <jakeicus@gmail.com>
2025-10-27 21:16:35 +0100

    - Seperated javascript files to correspond to use. - Added functionality for playoffs     - fixed drag issue     - added logic to decide whether the drop is valid     - added logic to remove the zones which become invalid after a valid drop

---
+++ src/CS2Pickems/wwwroot/js/stage.js
@@ -0,0 +1,42 @@
+﻿function drop(ev) {
+    ev.preventDefault();
+    const imageSrc = ev.dataTransfer.getData("imageSrc");
+    if (!imageSrc) return;
+
+    if (imageSrc.includes("unknown")) {
+        return;
+    }
+
+    let dropzone = ev.target;
+    if (!dropzone.classList.contains("match-dropzone-advanced") &&
+        !dropzone.classList.contains("match-dropzone-eliminated") && !dropzone.classList.contains("match")) {
+        dropzone = dropzone.closest(".match-dropzone-advanced, .match-dropzone-eliminated, .match");
+    }
+
+    placeImageInDropzone(imageSrc, dropzone, false);
+}
+
+function resetDropzoneStyle(dropzone) {
+    dropzone.removeAttribute("style"); // Removes inline styles
+
+    switch (dropzone.id) {
+        case "pick0":
+        case "pick1":
+            dropzone.textContent = "3-0";
+            break;
+        case "pick2":
+        case "pick3":
+        case "pick4":
+            dropzone.textContent = "3-1";
+            break;
+        case "pick5":
+        case "pick6":
+        case "pick7":
+            dropzone.textContent = "3-2";
+            break;
+        case "pick8":
+        case "pick9":
+            dropzone.textContent = "0-3";
+            break;
+    }
+}
