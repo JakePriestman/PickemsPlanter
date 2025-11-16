82a551cf05110aeeab0d62a64cd9fb46b8219d21
jake priestman <jakeicus@gmail.com>
2025-11-16 16:01:49 +0100

    Issue #44 - added html in _layout for a logout confirmation box - added css for the box - added javascript to toggle the box - change html for new functionality

--- src/PickemsPlanter/wwwroot/js/site.js
+++ src/PickemsPlanter/wwwroot/js/site.js
@@ -1,1 +1,8 @@
-﻿
+﻿function toggleLogoutConfirmation() {
+    const logoutConfirmationDiv = document.getElementById('logoutConfirmation');
+
+    if (logoutConfirmationDiv) {
+        //logoutConfirmationDiv.style.display = logoutConfirmationDiv.style.display === 'none' ? 'flex' : 'none';
+        logoutConfirmationDiv.classList.toggle('show');
+    }
+}
