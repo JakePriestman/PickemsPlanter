35d4925f9ee72687db88594c16b96fa185c3ce67
Jake Priestman <84022353+JakePriestman@users.noreply.github.com>
2026-05-26 08:37:39 +0200

    Update Overview.cshtml.cs

--- src/PickemsPlanter/Pages/Profile/Overview.cshtml.cs
+++ src/PickemsPlanter/Pages/Profile/Overview.cshtml.cs
@@ -126,7 +126,7 @@ public class OverviewModel(IUserEventsTableService tableStorageService, IUserPre
 	{
 		var events = await eventTableService.GetAllEventsAsync();
 
-		EventOptions = [.. events.Where(x => !x.Disbled).Select(x => new SelectListItem
+		EventOptions = [.. events.Where(x => !x.Disabled).Select(x => new SelectListItem
 		{
 			Text = x.Name,
 			Value = x.Id
